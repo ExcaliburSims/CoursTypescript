4b905d414a32fb550eef21de379540d1a711b5ab
ExcaliburSims <ximeonzilu@gmail.com>
2022-12-28 12:02:20 +0100

    feat: lessaon variables

---
+++ index.ts
@@ -0,0 +1,14 @@
+console.log("Yo!");
+
+////////////////// LES VARIABLES /////////////////////////////////
+
+let num1 = 2;
+let alpha: string;
+let bool: boolean;
+let num2: number;
+
+//Si on ne sait pas quelle valeur va être affectée nous utilisons des unions
+
+let numOrString: number | string;
+
+////////////////// LES TABLEAUX /////////////////////////////////
